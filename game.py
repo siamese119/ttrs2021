60782df33dac40ba998216e34c31e720aa864f5a
siamese119 <rtg692002@gmail.com>
2021-03-23 15:36:37 +0900

    깃 첫번째 커밋

---
+++ game.py
@@ -0,0 +1,11 @@
+print("게임을 시작하지")
+print("테트리스를 시작")
+print("1.오른쪽 2.왼쪽 3.블록 바꾸기")
+number = input("숫자를 입력하세요 : ")
+print("당신이 입력한 숫자 : ", number)
+if number == '1' :
+    print("오른쪽으로 이동")
+elif number == '2':
+    print("왼쪽으로 이동")
+elif number == '3':
+    print("블록바꾸기")
